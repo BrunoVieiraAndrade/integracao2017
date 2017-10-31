87ae182156615126f98659b90687bc328efec542
thealexandrelara <thealexandrelara@gmail.com>
2017-10-31 20:01:53 -0200

    Alterações para adequação à acessibilidade, inclusão da imagem do banner inicial, alterações visuais.

--- src/components/home/home-slider.js
+++ src/components/home/home-slider.js
@@ -12,31 +12,7 @@ class HomeSlider extends Component {
 
     return (
       <div className="slider-container rev_slider_wrapper" style={{ height: 530 }}>
-        <div id="revolutionSlider" className="slider rev_slider manual">
-          <ul>
-            <li data-transition="boxfade">
-              <img src={homeSlider1}
-                   alt=""
-                   data-bgposition="center bottom"
-                   data-bgfit="cover"
-                   data-bgrepeat="no-repeat"
-                   data-bgparallax="10"
-                   className="rev-slidebg"
-                   data-no-retina />
-            </li>
-            <li data-transition="boxfade">
-
-              <img src="img/slides/home-slider-2.jpg"
-                   alt=""
-                   data-bgposition="center bottom"
-                   data-bgfit="cover"
-                   data-bgrepeat="no-repeat"
-                   data-bgparallax="10"
-                   className="rev-slidebg"
-                   data-no-retina />
-            </li>
-          </ul>
-        </div>
+        <img src="img/slides/home-banner.jpg" />
       </div>
     );
   }
